7f46e774aad0b45168c4844a1d20a26ad4720492
Faissal <fhattou@simplon.co>
2018-12-15 15:18:09 +0100

    Prettier configurations

--- client/src/components/Dropdown/index.js
+++ client/src/components/Dropdown/index.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import DropdownButton from "./DropdownButton";
-import List from "./List";
+import DropdownButton from './DropdownButton';
+import List from './List';
 
 class Dropdown extends Component {
   state = {
     isDropdownOpen: false,
-    selectedDropdownItem: null
+    selectedDropdownItem: null,
   };
 
   toggleDropdown = () => {
@@ -21,7 +21,7 @@ class Dropdown extends Component {
     const { isDropdownOpen, selectedDropdownItem } = this.state;
     const { listItems, text } = this.props;
     return (
-      <div className="Dropdown">
+      <div className='Dropdown'>
         <DropdownButton
           isDropdownOpen={isDropdownOpen}
           selectedDropdownItem={selectedDropdownItem}
@@ -29,10 +29,7 @@ class Dropdown extends Component {
           toggleDropdown={this.toggleDropdown}
         />
         {isDropdownOpen && (
-          <List
-            listItems={listItems}
-            selectDropdownItem={this.selectDropdownItem}
-          />
+          <List listItems={listItems} selectDropdownItem={this.selectDropdownItem} />
         )}
       </div>
     );
