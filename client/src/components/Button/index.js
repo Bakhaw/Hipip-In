879eb33c87d1bd95b58279a47c778d16db21883e
Faissal <fhattou@simplon.co>
2018-12-16 14:51:13 +0100

    Added a loading props to <Button />

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import classNames from 'classnames';
+
 import DoubleChevronRight from '../../assets/images/layout/double-chevron-right.png';
-
 import Fab from '@material-ui/core/Fab';
+import Spinner from '../Spinner';
 
-function CustomButton({ disabled, onClick, style, text }) {
+function CustomButton({ disabled, isLoading, onClick, style, text }) {
   return (
     <Fab className='CustomButton' disabled={disabled} onClick={onClick} style={style}>
-      <p>{text}</p>
-      <img
-        alt='Double chevron droite'
-        className={classNames(
-          'CustomButton__icon',
-          disabled && 'CustomButton__icon__disabled'
-        )}
-        src={DoubleChevronRight}
-      />
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <p>{text}</p>
+          <img
+            alt='Double chevron droite'
+            className={classNames(
+              'CustomButton__icon',
+              disabled && 'CustomButton__icon__disabled'
+            )}
+            src={DoubleChevronRight}
+          />
+        </Fragment>
+      )}
     </Fab>
   );
 }
